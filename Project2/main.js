6725206a124a1cb42c27795f3be477d858840336
Babin6139 <khatribabin39@gmail.com>
2023-02-11 13:39:54 +0545

    feat:implemented multiple objects synchronization

--- Project2/main.js
+++ Project2/main.js
@@ -3,4 +3,6 @@
 // gsap.set(".logo,.autocad",{transformOrigin:"50% 50%"})
 // gsap.to(".logo,.autocad",{duration:20, rotation:360})
 
-gsap.from(".logo",{duration:1.5, opactiy:0, scale:0.5, ease:"back"})
+// gsap.from(".logo",{duration:1.5, opactiy:0, scale:0.5, ease:"back"})
+
+gsap.from(".circle",{duration:1, opacity:0, y:"random(-200,200)", stagger:0.25})
